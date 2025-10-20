55ee8ea7ae0d4a7215a3b0872742d8de5563ef7c
eedu7 <mueedahmad067@gmail.com>
2025-10-20 07:48:02 +0500

    Added some settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function HomePage() {
-  return (
-    <div className="text-bold bg-zinc-50 w-screen h-screen grid place-items-center">
-      <div className="shadow-lg p-12 rounded-md">HomePage</div>
-    </div>
-  );
+    return (
+        <div className="text-bold bg-zinc-50 w-screen h-screen grid place-items-center">
+            <Card>
+                <CardContent>
+                    <h1>Hello, World</h1>
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
